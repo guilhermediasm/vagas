26284df27d16150c23ba1705e13f2573024907a1
Guilherme Dias <guilherme.gdm1@gmail.com>
2023-06-11 13:15:58 -0300

    fix: Test2 correction

--- src/teste2.js
+++ src/teste2.js
@@ -1,17 +1,41 @@
-var data =  require("./fakeData");
+const fs = require('fs');
+const path = require('path');
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
-    var newUser = {
-        name: name,
-        job: job,
+const dataPath = path.join(__dirname, 'fakeData.json');
+
+module.exports = function (req, res) {
+  const name = req.body.name;
+  const job = req.body.job;
+
+  const newUser = {
+    name: name,
+    job: job,
+  };
+
+  // Lê o conteúdo atual do arquivo fakeData.json
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
     }
 
-    data.push(newUser)
-    
-    res.send(newUser);
+    // Faz o parse do conteúdo do arquivo para um objeto JSON
+    const users = JSON.parse(data);
 
+    // Adiciona o novo usuário ao array existente
+    users.push(newUser);
+
+    // Escreve o conteúdo atualizado no arquivo fakeData.json
+    fs.writeFile(dataPath, JSON.stringify(users), 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      // Retorna o novo usuário como resposta
+      res.send(newUser);
+    });
+  });
 };
